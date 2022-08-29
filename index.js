a020a7d088c28b88bc6425010c033e96a5be35e4
DevelopMeLaya <104338734+DevelopMeLaya@users.noreply.github.com>
2022-08-29 11:45:09 -0400

    Created js

---
+++ index.js
@@ -0,0 +1,57 @@
+function see(event) {
+  event.preventDefault();
+
+  let anotherCity = document.querySelector("#another-city");
+
+  let cityName = document.querySelector("#city-name");
+  cityName.innerHTML = `${anotherCity.value}`;
+}
+let form = document.querySelector("#city-form");
+
+form.addEventListener("submit", see);
+
+let now = new Date();
+
+let currentDate = document.querySelector("#dateTime");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let day = days[now.getDay()];
+
+const str = new Date(2022, 1, 1).toLocaleString("en-US", {
+  hour: "numeric",
+  minute: "numeric",
+  hour12: true
+});
+console.log(str);
+let hours = now.getHours();
+let minutes = now.getMinutes();
+minutes = minutes.toString().padStart(2, "0");
+
+currentDate.innerHTML = `${day} ${hours}:${minutes}`;
+
+function fahrenheit(event) {
+  event.preventDefault();
+
+  let currentTemp = document.querySelector("#current-temp");
+  currentTemp.innerHTML = "87";
+}
+let fDegree = document.querySelector("#f-degree");
+fDegree.addEventListener("click", fahrenheit);
+
+function celsius(event) {
+  event.preventDefault();
+
+  let currentTemp = document.querySelector("#current-temp");
+  currentTemp.innerHTML = "30";
+}
+let cDegree = document.querySelector("#c-degree");
+cDegree.addEventListener("click", celsius);
